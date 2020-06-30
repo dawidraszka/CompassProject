3628258a2474fa9fb03c9e7d087fdd734eac82b5
Dawid Raszka <raszka.dawid@gmail.com>
2020-06-30 12:02:15 +0200

    Add location permission request; add destination to UI

--- app/src/main/java/pl/dawidraszka/compassproject/CompassActivity.kt
+++ app/src/main/java/pl/dawidraszka/compassproject/CompassActivity.kt
@@ -1,11 +1,66 @@
 package pl.dawidraszka.compassproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_compass.*
 
-class CompassActivity : AppCompatActivity() {
+const val PERMISSION_REQUEST_LOCATION = 0
+
+class CompassActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compass)
+
+        set_destination_button.setOnClickListener {
+            if (checkSelfPermissionCompat(Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO ADD SETTING DESTINATION
+            } else {
+                requestLocationPermission()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // TODO ADD SETTING DESTINATION
+            } else {
+                Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (shouldShowRequestPermissionRationaleCompat(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            Snackbar.make(
+                set_destination_button,
+                R.string.location_permission_alert_message,
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction(
+                R.string.ok
+            ) {
+                requestPermissionsCompat(
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    PERMISSION_REQUEST_LOCATION
+                )
+            }.show()
+
+        } else {
+            requestPermissionsCompat(
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                PERMISSION_REQUEST_LOCATION
+            )
+        }
     }
 }
