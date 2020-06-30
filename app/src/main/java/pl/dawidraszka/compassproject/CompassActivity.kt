80631a7c707a3f39d5acb0ab4a5565928864721d
Dawid Raszka <raszka.dawid@gmail.com>
2020-06-30 11:01:24 +0200

    Add graphics and basic UI

--- app/src/main/java/pl/dawidraszka/compassproject/CompassActivity.kt
+++ app/src/main/java/pl/dawidraszka/compassproject/CompassActivity.kt
@@ -3,9 +3,9 @@ package pl.dawidraszka.compassproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CompassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_compass)
     }
 }
